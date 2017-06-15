a51f3c4fea671434598ca560cd2aaf0025ccc062
bredele <olivier.wietrich@gmail.com>
2017-06-15 11:56:19 -0600

    Collect buffer from incoming messages

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
+/**
+ * Dependencies.
+ */
+
+const url = require('url').parse
+const query = require('querystring').parse
+
 
 /**
  * This is a simple description.
  *
+ * @param {Object} methods
  * @api public
  */
 
-module.exports = function () {
-  // do something
+module.exports = function (methods) {
+  return (req) => {
+    const type = req.method.toLowerCase()
+    const params = query(url(req.url).query)
+    collect(req, buffer => {
+      const data = buffer.length ? parse(buffer, req) : null
+      methods[type](params, data)
+    })
+  }
 }
 
-  
+
+/**
+ * Collect HTTP incoming buffers.
+ *
+ * @param {HttpIncomingMessage} req
+ * @param {Function} cb
+ * @api private
+ */
+
+function collect (req, cb) {
+  let list = []
+  req.on('data', (chunk) => list.push(chunk))
+  req.on('end', () => cb(Buffer.concat(list)))
+}
+
+
+/**
+ * Parse request buffer according its encoded format.
+ *
+ * @param {Buffer} buffer
+ * @param {HttpIncomingMessage} req
+ * @return {Object}
+ * @api private
+ */
+
+function parse (buffer, req) {
+  return JSON.parse(buffer.toString())
+}
--- test/manner.js
+++ test/manner.js
@@ -1,13 +1,54 @@
-
 /**
  * Test dependencies.
  */
 
 const test = require('tape')
+const service = require('..')
+const Stream = require('stream').Readable
 
-test('this is an example', assert => {
+
+test('get params from GET incoming message', assert => {
   assert.plan(1)
-  assert.equal(1 + 2, 3)
+  const api = service({
+    'get': (params) => {
+      assert.deepEqual(params, {
+        label: 'hello'
+      })
+    }
+  })
+  api(request('GET', 'label=hello'))
 })
 
-  
+
+test('get data from POST incoming message', assert => {
+  assert.plan(1)
+  const message = {
+    foo: 'bar'
+  }
+  const api = service({
+    'post': (params, data) => {
+      assert.deepEqual(data, message)
+    }
+  })
+  api(request('POST', 'label=hello', message))
+})
+
+
+/**
+ * Simulate HTTP request.
+ *
+ * @param {String} method
+ * @param {String} params
+ * @param {Object} data
+ * @api private
+ */
+
+function request (method, params, data) {
+  const req = new Stream
+  req._read = function () {}
+  req.method = method
+  req.url = '?' + params
+  req.push(JSON.stringify(data))
+  req.push(null)
+  return req
+}
