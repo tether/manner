1ec7391dab749bd04c8b7b19d86a8d0ae00b3a25
bredele <olivier.wietrich@gmail.com>
2017-06-15 12:00:54 -0600

    Handle undefined queries

--- test/manner.js
+++ test/manner.js
@@ -19,6 +19,16 @@ test('get params from GET incoming message', assert => {
   api(request('GET', 'label=hello'))
 })
 
+test('GET method should return empty object if no params has been specified', assert => {
+  assert.plan(1)
+  const api = service({
+    'get': (params) => {
+      assert.deepEqual(params, {})
+    }
+  })
+  api(request('GET'))
+})
+
 
 test('get data from POST incoming message', assert => {
   assert.plan(1)
@@ -32,7 +42,6 @@ test('get data from POST incoming message', assert => {
   })
   api(request('POST', 'label=hello', message))
 })
-
 
 /**
  * Simulate HTTP request.
