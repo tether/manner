ed6f0d413e39a8e314702bb5bd3da3e07f39f3fb
bredele <olivier.wietrich@gmail.com>
2017-07-16 10:01:30 -0600

    Return straight up values

--- index.js
+++ index.js
@@ -3,15 +3,27 @@
  */
 
 const url = require('url').parse
-const query = require('querystring').parse
+const salute = require('salute')
 
 
 /**
  *
  */
 
-module.exports = function () {
+module.exports = function (obj) {
+  const methods = routes(obj)
   return (req, res) => {
-    
+    return methods[req.method.toLowerCase()](req, res)
   }
+}
+
+
+function routes (methods) {
+  const result = {}
+  Object.keys(methods).map(key => {
+    result[key] = salute((req, res) => {
+      return methods[key]
+    })
+  })
+  return result
 }
--- test/manner.js
+++ test/manner.js
@@ -10,8 +10,31 @@ const Writable = require('stream').Writable
 const server = require('server-test')
 
 
-test('should return a function', assert => {
+test('should be a high order function', assert => {
   assert.plan(1)
-  const api = service()
+  const api = service({})
   assert.equal(typeof api, 'function')
+})
+
+// test('should return a readable stream when passed a http request and response', assert => {
+//   assert.plan(1)
+//   const api = service()
+//   server((req, res) => {
+//     const stream = api(req, res)
+//     assert.equal(stream instanceof Readable, true)
+//   })
+//
+// })
+
+
+test('should return value from defined method', assert => {
+  assert.plan(1)
+  const api = service({
+    get: 'hello world!'
+  })
+  server((req, res) => {
+    api(req, res).pipe(concat(data => {
+      assert.equal(data.toString(), 'hello world!')
+    }))
+  })
 })
