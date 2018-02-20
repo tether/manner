bb60e742ff6244593685a01c7cd06f8f293b4731
bredele <olivier.wietrich@gmail.com>
2018-02-20 14:22:09 -0700

    Send promise through http response

--- test/http.js
+++ test/http.js
@@ -6,6 +6,7 @@ const test = require('tape')
 const service = require('..')
 const http = require('server-test')
 const concat = require('concat-stream')
+const fs = require('fs')
 
 
 test('should stream service', assert => {
@@ -55,10 +56,47 @@ test('should stream error 501 if method not implemented', assert => {
       error: {
         status: 501,
         message: 'method GET not implemented',
-        payload: {}  
+        payload: {}
       }
     })
     assert.equal(res.statusCode, 501)
+  })
+})
+
+
+test('should not have to return data', assert => {
+  assert.plan(1)
+  const api = service({
+    get: () => {
+      assert.ok('service executed')
+    }
+  })
+  server(api, (data, res) => {})
+})
+
+
+test('should chunk streams through the respsonse', assert => {
+  assert.plan(1)
+  const api = service({
+    get: () => fs.createReadStream(__dirname + '/manner.txt')
+  })
+  server(api, (data, res) => {
+    assert.equal(data.toString(), 'hello world\n')
+  })
+})
+
+
+test('should send promise through the respsonse', assert => {
+  assert.plan(1)
+  const api = service({
+    get: () => {
+      return new Promise((resolve) => {
+        setTimeout(() => resolve('hello world'), 30)
+      })
+    }
+  })
+  server(api, (data, res) => {
+    assert.equal(data.toString(), 'hello world')
   })
 })
 
