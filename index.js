8f95d8e3e8a39f9dc6d59f891fd421e038f58ef9
bredele <olivier.wietrich@gmail.com>
2018-02-20 10:34:49 -0700

    Parse and transform object into resource services

--- index.js
+++ index.js
@@ -6,18 +6,46 @@ const methodd = require('methodd')
 
 
 /**
- * Create web services from an object.
+ * Create web resource.
  *
  * @param {Object} obj
- * @param {Object} schema
  * @return {Function}
  * @api public
  */
 
-module.exports = (services) => {
+module.exports = (obj) => {
   const resource = methodd()
-  Object.keys(services).map(name => {
-    resource.add(name, '/', services[name])
+  const services = parse(obj)
+  Object.keys(services).map(method => {
+    const service = services[method]
+    Object.keys(service).map(path => {
+      const handler = service[path]
+      resource.add(method, path, handler.service)
+    })
   })
   return resource
+}
+
+
+/**
+ * Parse services.
+ *
+ * @param {Object} services
+ * @api private
+ */
+
+function parse (services) {
+  const result = {}
+  Object.keys(services).map(name => {
+    const service = services[name]
+    if (typeof service === 'function') {
+      result[name] = {
+        '/' : {
+          service,
+          data: {}
+        }
+      }
+    }
+  })
+  return result
 }
